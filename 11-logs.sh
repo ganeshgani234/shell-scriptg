5ca8a65017131e9cea4fb9c80818f9717f11ba44
ganeshgani234 <ganeshaggidi009@gmail.com>
2024-01-11 19:18:47 +0530

    conditions

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,22 +3,25 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "ERTOR:: $2 ... FAILED"
+        echo "ERTOR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 ... success"
+        echo "$2 ... $G success $N"
     fi
 
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR:: please run this script with root access"
+    echo -e "$R ERROR:: please run this script with root access $N"
     exit 1 # you can give other than 0
 else 
     echo "you are root user"
