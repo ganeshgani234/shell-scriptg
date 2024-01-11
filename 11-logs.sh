41128bea4f8db4f1bf1c1827391359e4467f12f3
ganeshgani234 <ganeshaggidi009@gmail.com>
2024-01-11 19:23:49 +0530

    conditions

--- 11-logs.sh
+++ 11-logs.sh
@@ -12,10 +12,10 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "ERTOR:: $2 ... $R FAILED $N"
+        echo -e "ERTOR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 ... $G success $N"
+        echo -e "$2 ... $G success $N"
     fi
 
 }
