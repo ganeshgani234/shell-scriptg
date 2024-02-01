77e3ff58ce4e1dac7a0517f7b35c067cbcf195eb
ganeshgani234 <ganeshaggidi009@gmail.com>
2024-02-01 12:52:42 +0530

    added script

---
+++ 13-install-packages.sh
@@ -0,0 +1,13 @@
+!#bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R ERROR:: please run this script with root access $N"
+    exit 1 # you can give other than 0
+else 
+    echo "you are root user"
+fi
+
+echo "All arguments passed: $@"
